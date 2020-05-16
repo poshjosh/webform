2c0184fd45372c9bbb7e5bf8db27244404b5b0eb
poshjosh <posh.bc@gmail.com>
2020-05-17 00:21:03 +0100

    Add base package class holder for spring boot

---
+++ src/main/java/com/looseboxes/webform/WebformBasePackageClass.java
@@ -0,0 +1,10 @@
+package com.looseboxes.webform;
+
+/**
+ * Do not rename this class. It could break applications/libraries that use
+ * this library.
+ * @author chinomso ikwuagwu
+ */
+public final class WebformBasePackageClass {
+    private WebformBasePackageClass() {}
+}
