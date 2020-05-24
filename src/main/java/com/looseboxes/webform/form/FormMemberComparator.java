895e76d071fda6199ce481f4853bd238a4d5e9ff
poshjosh <posh.bc@gmail.com>
2020-05-24 22:11:18 +0100

    Add type variable

--- src/main/java/com/looseboxes/webform/form/FormMemberComparator.java
+++ src/main/java/com/looseboxes/webform/form/FormMemberComparator.java
@@ -2,10 +2,11 @@ package com.looseboxes.webform.form;
 
 import java.util.Comparator;
 import com.bc.webform.FormMember;
+import java.lang.reflect.Field;
 
 /**
  * @author hp
  */
-public interface FormMemberComparator extends Comparator<FormMember>{
+public interface FormMemberComparator extends Comparator<FormMember<Field, Object>>{
     
 }
