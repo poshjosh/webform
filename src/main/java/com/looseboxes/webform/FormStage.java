d1522195e4f4331a1e124574c6cc350e1bd104a0
poshjosh <posh.bc@gmail.com>
2020-06-06 18:18:40 +0100

    Add FormStage

---
+++ src/main/java/com/looseboxes/webform/FormStage.java
@@ -0,0 +1,11 @@
+package com.looseboxes.webform;
+
+/**
+ * @author hp
+ */
+public interface FormStage {
+    String dependents = "dependents";
+    String validateSingle = "validateSingle";
+    String validate = "validate";
+    String submit = "submit";
+}
