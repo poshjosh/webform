f3f066550bb554c67e9c3aba5afdd678f211b492
poshjosh <posh.bc@gmail.com>
2020-07-28 16:06:35 +0100

    No longer extend both Converter and Printer
    Spring complained when this class extended both Converter and Printer
    The error stack trace is reproduced below.
    
    To fix the problem we no longer extend Converter and Printer as that
    was just done for convenience
    
    ```
    Caused by: org.springframework.beans.BeanInstantiationException: Failed to instantiate [org.springframework.format.support.FormattingConversionService]: Factory method 'mvcConversionService' threw exception; nested exception is java.lang.IllegalArgumentException: Unable to determine source type <S> and target type <T> for your Converter [com.looseboxes.webform.converters.EntityToSelectOptionConverterImpl]; does the class parameterize those types?
    	at org.springframework.beans.factory.support.SimpleInstantiationStrategy.instantiate(SimpleInstantiationStrategy.java:185)
    	at org.springframework.beans.factory.support.ConstructorResolver.instantiate(ConstructorResolver.java:651)
    	... 98 common frames omitted
    Caused by: java.lang.IllegalArgumentException: Unable to determine source type <S> and target type <T> for your Converter [com.looseboxes.webform.converters.EntityToSelectOptionConverterImpl]; does the class parameterize those types?
    	at org.springframework.core.convert.support.GenericConversionService.addConverter(GenericConversionService.java:92)
    	at org.springframework.boot.convert.ApplicationConversionService.addBeans(ApplicationConversionService.java:166)
    	at org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration$WebMvcAutoConfigurationAdapter.addFormatters(WebMvcAutoConfiguration.java:295)
    	at org.springframework.web.servlet.config.annotation.WebMvcConfigurerComposite.addFormatters(WebMvcConfigurerComposite.java:81)
    	at org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration.addFormatters(DelegatingWebMvcConfiguration.java:78)
    	at org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration$EnableWebMvcConfiguration.mvcConversionService(WebMvcAutoConfiguration.java:428)
    ```

--- src/main/java/com/looseboxes/webform/converters/EntityToSelectOptionConverter.java
+++ src/main/java/com/looseboxes/webform/converters/EntityToSelectOptionConverter.java
@@ -4,26 +4,20 @@ import com.bc.webform.choices.SelectOption;
 import com.bc.webform.choices.SelectOptionImpl;
 import java.util.Locale;
 import java.util.function.BiFunction;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.format.Printer;
 
 /**
  * @author hp
  */
 public interface EntityToSelectOptionConverter<V> extends 
-        BiFunction<Object, Locale, SelectOption<V>>,
-        Converter<Object, V>,
-        Printer<Object>{
-
+        BiFunction<Object, Locale, SelectOption<V>>{
+    
     @Override
     default SelectOption<V> apply(Object source, Locale locale) {
         return new SelectOptionImpl(convert(source), print(source, locale));
     }
     
-    @Override
     V convert(Object source);
     
-    @Override
     default String print(Object source, Locale locale) {
         return String.valueOf(source);
     }
