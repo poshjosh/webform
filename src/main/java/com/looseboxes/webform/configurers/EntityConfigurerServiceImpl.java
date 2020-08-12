6950811e1ee344d08d8aec1598bb0a9cdaed3865
poshjosh <posh.bc@gmail.com>
2020-08-12 11:31:24 +0100

    Use the Class argument as key before search for assignable type to use

--- src/main/java/com/looseboxes/webform/configurers/EntityConfigurerServiceImpl.java
+++ src/main/java/com/looseboxes/webform/configurers/EntityConfigurerServiceImpl.java
@@ -18,21 +18,23 @@ public class EntityConfigurerServiceImpl implements EntityConfigurerService{
     @Override
     public <T> Optional<EntityConfigurer<T>> getConfigurer(Class<T> type) {
         
-        type = getMatchingKey(type);
+        EntityConfigurer entityConfigurer = this.configurers.get(type);
         
-        final EntityConfigurer entityConfigurer = this.configurers.get(type);
+        if(entityConfigurer == null) {
+            
+            final Class assignableType = this.getAssignableType(type).orElse(null);
+            
+            if(assignableType != null) {
+                
+                entityConfigurer = this.configurers.get(assignableType);
+            }
+        }
         
         return Optional.ofNullable(entityConfigurer);
     }
     
-    private Class getMatchingKey(Class type) {
-        for(Class cls : this.configurers.keySet()) {
-            if(cls.isAssignableFrom(type)) {
-                type = cls;
-                break;
-            }
-        }
-        return type;
+    private Optional<Class> getAssignableType(final Class type) {
+        return configurers.keySet().stream().filter((cls) -> cls.isAssignableFrom(type)).findAny();
     }
 
     @Override
