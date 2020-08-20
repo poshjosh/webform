81832c13672809d4f04be97de05052b9a52cb592
poshjosh <posh.bc@gmail.com>
2020-08-20 13:29:36 +0100

    Remove data manipulation methods to make this a purely data object

--- src/main/java/com/looseboxes/webform/web/WebRequest.java
+++ src/main/java/com/looseboxes/webform/web/WebRequest.java
@@ -1,13 +1,8 @@
 package com.looseboxes.webform.web;
 
-import com.looseboxes.webform.services.AttributeService;
-import com.looseboxes.webform.store.AttributeStore;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -20,20 +15,6 @@ public interface WebRequest<T> {
     String getParameter(String name);
     
     String [] getParameterValues(String name);
-    
-    default AttributeStore<ModelMap> modelAttributes() {
-        return this.getAttributeService().modelAttributes();
-    }
-    
-    default AttributeStore<HttpServletRequest> requestAttributes() {
-        return this.getAttributeService().requestAttributes();
-    }
-
-    default AttributeStore<HttpSession> sessionAttributes() {
-        return this.getAttributeService().sessionAttributes();
-    }
-
-    AttributeService getAttributeService();
     
     boolean hasFiles();
     
